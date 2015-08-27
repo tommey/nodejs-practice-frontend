f1d4079858d55321bc12433fcb6148b6bdfdcfa5
Tamas Toth <tommey@tommey.hu>
2015-08-27 09:28:04 -0400

    Added more detailed Google selenium test

--- e2e/index.js
+++ e2e/index.js
@@ -1,7 +1,18 @@
 module.exports = {
 	'Check if Google works': function(browser) {
-		browser.url('https://google.com')
-			.pause(5000)
+		browser
+			.url('https://google.com')
+			.waitForElementVisible('body', 1000)
+			.assert.title('Google')
+			.aasert.visible('input[type=text]')
+			.setValue('input[type=text]', 'rembrandt van rijn')
+			.waitForElementVisible('button[name=btnG]', 1000)
+			.click('button[name=btnG]')
+			.pause(1000)
+			.assert.containsText(
+				'ol#rso li:first-child', 
+				'Rembrandt - Wikipedia, the free encyclopedia'
+			)
 			.end();
 	}
 };
